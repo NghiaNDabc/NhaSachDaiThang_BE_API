152492028f7c4506b47abe80b2335a3478af19fb
NghiaNDabc <203nghiapham@gmail.com>
2024-10-27 23:17:15 +0700

    Confix db and jwt

---
+++ Configurations/DbContextConfiguration.cs
@@ -0,0 +1,15 @@
+﻿// DbContextConfiguration.cs
+using Microsoft.EntityFrameworkCore;
+using NhaSachDaiThang_BE_API.Data;
+
+namespace NhaSachDaiThang_BE_API.Configurations
+{
+    public static class DbContextConfiguration
+    {
+        public static void AddDbContextConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BookStoreContext>(options =>
+                options.UseLazyLoadingProxies().UseSqlServer(configuration.GetConnectionString("BookStoreDb")));
+        }
+    }
+}
---
+++ Configurations/JwtConfiguration.cs
@@ -0,0 +1,37 @@
+﻿// JwtConfiguration.cs
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using NhaSachDaiThang_BE_API.Models.Dtos;
+
+namespace NhaSachDaiThang_BE_API.Configurations
+{
+    public static class JwtConfiguration
+    {
+        public static void AddJwtConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            services.Configure<JwtSettings>(jwtSettings);
+
+            var secretKey = jwtSettings["SecretKey"];
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+            });
+        }
+    }
+}
