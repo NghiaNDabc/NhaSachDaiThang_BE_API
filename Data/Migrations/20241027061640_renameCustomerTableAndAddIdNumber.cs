a4c887ce36900ae7e52038aa609dcd5dfa379a3b
NghiaNDabc <203nghiapham@gmail.com>
2024-10-27 13:30:40 +0700

    delete emp table, and reanme customer table

---
+++ Data/Migrations/20241027061640_renameCustomerTableAndAddIdNumber.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NhaSachDaiThang_BE_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class renameCustomerTableAndAddIdNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK__Orders__Customer__5DCAEF64",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK__Reviews__Custome__70DDC3D8",
+                table: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    IdNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    PasswordHash = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime", nullable: true, defaultValueSql: "(getdate())"),
+                    CreatedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ModifyDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    ModifyBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    RoleID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Customer__A4AE64B80DDF41BD", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK__Customers__RoleI__534D60F1",
+                        column: x => x.RoleID,
+                        principalTable: "Roles",
+                        principalColumn: "RoleID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_RoleID",
+                table: "User",
+                column: "RoleID");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Customer__A9D1053471732474",
+                table: "User",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK__Orders__Customer__5DCAEF64",
+                table: "Orders",
+                column: "CustomerID",
+                principalTable: "User",
+                principalColumn: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK__Reviews__Custome__70DDC3D8",
+                table: "Reviews",
+                column: "CustomerID",
+                principalTable: "User",
+                principalColumn: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK__Orders__Customer__5DCAEF64",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK__Reviews__Custome__70DDC3D8",
+                table: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleID = table.Column<int>(type: "int", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime", nullable: true, defaultValueSql: "(getdate())"),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ModifyBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ModifyDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    PasswordHash = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Customer__A4AE64B80DDF41BD", x => x.CustomerID);
+                    table.ForeignKey(
+                        name: "FK__Customers__RoleI__534D60F1",
+                        column: x => x.RoleID,
+                        principalTable: "Roles",
+                        principalColumn: "RoleID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleID",
+                table: "Users",
+                column: "RoleID");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Customer__A9D1053471732474",
+                table: "Users",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK__Orders__Customer__5DCAEF64",
+                table: "Orders",
+                column: "CustomerID",
+                principalTable: "Users",
+                principalColumn: "CustomerID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK__Reviews__Custome__70DDC3D8",
+                table: "Reviews",
+                column: "CustomerID",
+                principalTable: "Users",
+                principalColumn: "CustomerID");
+        }
+    }
+}
---
+++ Data/Migrations/20241027062704_DeleteEmployeeTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NhaSachDaiThang_BE_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteEmployeeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleID = table.Column<int>(type: "int", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime", nullable: true, defaultValueSql: "(getdate())"),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ModifyBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ModifyDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    PasswordHash = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Employee__7AD04FF1F64003B7", x => x.EmployeeID);
+                    table.ForeignKey(
+                        name: "FK__Employees__RoleI__5812160E",
+                        column: x => x.RoleID,
+                        principalTable: "Roles",
+                        principalColumn: "RoleID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_RoleID",
+                table: "Employees",
+                column: "RoleID");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Employee__A9D10534FA126257",
+                table: "Employees",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+        }
+    }
+}
