85e44c2846d04cca7a81e27b9c56de715b8a6e84
NghiaNDabc <203nghiapham@gmail.com>
2024-10-28 11:53:12 +0700

    remove Otp in table User

---
+++ Data/Migrations/20241028040107_AddUserNameToTableUser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NhaSachDaiThang_BE_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserNameToTableUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OtpCode",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "OtpExpiryTime",
+                table: "User");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "User",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "User");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtpCode",
+                table: "User",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OtpExpiryTime",
+                table: "User",
+                type: "datetime",
+                nullable: true);
+        }
+    }
+}
